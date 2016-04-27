e2e89b237e3b2a1de18382d3a00a3fe60ef69433
Maftouh <maftouh.hassane@gmail.com>
2016-04-27 14:36:50 +0200

    Ecriture des relation entre les tables

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,45 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    /**
+     * Model relationship
+     * 
+     * @return type
+     */
+    
+    public function user(){
+        return $this->belongsTo('App\User');
+    }
+    
+    public function category(){
+        return $this->belongsTo('App\Category');
+    }
+    
+    public function tag(){
+        return $this->belongsToMany('App\Tag');
+    }
+    
+    public function picture(){
+        return $this->hasOne('App\Picture');
+    }
+    
+    /**
+     * Field set configuration
+     * 
+     * @return datatype
+     */
+    
+    protected $filables=[
+        'field1'
+    ];
+    
+    /**
+     * Carbone Date configuration
+     * 
+     * @return datatype
+     */
+    
+    protected $date=[
+        'dateField'
+    ];
 }

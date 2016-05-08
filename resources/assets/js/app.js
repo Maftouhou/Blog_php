771be160afd4976b7904ad5b4aaa65e18bfee259
maftouh <maftouh.hassane@gmail.com>
2016-05-08 18:16:47 +0200

    reorgatisation des migration pour une execution plus sure

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,15 +1,24 @@
 (function($){
     'use strict';
     
+    /**
+     * Fermeture du message des action du PostController
+     */
     $('.action_response > span').on('click', function(){
         $(this).parent().hide();
     });
     
+    /**
+     * Lancemnet de la popin de confirmation de suppression d'un article
+     */
     $('.delete_post_request').on('click', function(){
         var delete_form = $(this).next('.delete_post_confirmation');
         modal(delete_form);
     });
     
+    /**
+     * Fermeture de la popin de confirmation de suppression d'un article
+     */
     $('.canncel_delete').on('click', function(){
         $(this).trigger('closeModal');
     });
